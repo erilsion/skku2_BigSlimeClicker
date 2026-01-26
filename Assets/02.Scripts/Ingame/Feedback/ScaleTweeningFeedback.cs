e735a93b4d253adf231ed93fb7cf873a7fa9eff5
erilsion <erilsion@gmail.com>
2026-01-26 16:22:26 +0900

    chore: 트위닝 크기 및 느낌 조절

--- Assets/02.Scripts/Ingame/Feedback/ScaleTweeningFeedback.cs
+++ Assets/02.Scripts/Ingame/Feedback/ScaleTweeningFeedback.cs
@@ -5,8 +5,11 @@ public class ScaleTweeningFeedback : MonoBehaviour, IFeedback
 {
     [SerializeField] private ClickSlime _owner;
 
-    private float _endScaleValue = 1.3f;
-    private float _scaleDuration = 0.14f;
+    private float _startPressX = 1.4f;
+    private float _startPressY = 0.7f;
+    private float _pressedDuration = 0.1f;
+    private float _stayDuration = 0.04f;
+    private float _endDuration = 0.24f;
 
     private void Awake()
     {
@@ -21,11 +24,13 @@ public class ScaleTweeningFeedback : MonoBehaviour, IFeedback
     // 역할: 스케일 트위닝 피드백에 대한 로직 담당
     public void PlayTween()
     {
-        _owner.transform.DOKill();
+        transform.DOKill();
 
         Sequence sequence = DOTween.Sequence();
-        sequence.Append(_owner.transform.DOScale(_endScaleValue, _scaleDuration).SetEase(Ease.OutBack));
-        sequence.Append(_owner.transform.DOScale(1f, _scaleDuration).SetEase(Ease.InOutQuad));
+
+        sequence.Append(transform.DOScale(new Vector2(_startPressX, _startPressY), _pressedDuration).SetEase(Ease.OutQuad));
+        sequence.AppendInterval(_stayDuration);
+        sequence.Append(transform.DOScale(Vector2.one, _endDuration).SetEase(Ease.OutElastic));
     }
 
 }
