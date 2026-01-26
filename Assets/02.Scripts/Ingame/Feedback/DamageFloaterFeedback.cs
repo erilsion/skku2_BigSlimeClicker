97c4976a40696e281f8d7f2d0bf71a16575f2893
erilsion <erilsion@gmail.com>
2026-01-26 17:36:19 +0900

    feat: 데미지 플로팅 관련 클래스 생성

--- Assets/02.Scripts/Ingame/Feedback/DamageFloaterFeedback.cs
+++ Assets/02.Scripts/Ingame/Feedback/DamageFloaterFeedback.cs
@@ -2,8 +2,36 @@
 
 public class DamageFloaterFeedback : MonoBehaviour, IFeedback
 {
+    [Header("Pool")]
+    [SerializeField] private DamageFloaterPool _pool;
+
+    [Header("Spawn Offset")]
+    [SerializeField] private Vector2 _randomOffset = new(0.25f, 0.15f);
+
+    private bool _init;
+
+    private void Awake()
+    {
+        // 풀 루트는 이 오브젝트 밑으로 정리한다.
+        _pool.Initialize(transform);
+        _init = true;
+    }
+
     public void Play(ClickInfo clickInfo)
     {
-        // 필수 과제: 데미지 플로팅 구현
+        if (!_init) _pool.Initialize(transform);
+
+        Vector2 offset = new(
+            Random.Range(-_randomOffset.x, _randomOffset.x),
+            Random.Range(-_randomOffset.y, _randomOffset.y)
+        );
+
+        Vector3 position = (Vector3)(clickInfo.Position + offset);
+
+        var floater = _pool.Get(position);
+        if (floater != null)
+        {
+            floater.Play(clickInfo.Damage);
+        }
     }
 }
