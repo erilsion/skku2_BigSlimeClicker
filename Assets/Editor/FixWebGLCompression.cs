20fb3c9c36216eae8787490205ea32c18bb9b62d
erilsion <erilsion@gmail.com>
2026-02-10 09:56:37 +0900

    Fix: githubio 사이트 에러 수정

---
+++ Assets/Editor/FixWebGLCompression.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class FixWebGLCompression
+{
+    [MenuItem("Tools/Fix WebGL Compression Settings")]
+    public static void FixCompression()
+    {
+        PlayerSettings.WebGL.compressionFormat = WebGLCompressionFormat.Disabled;
+        PlayerSettings.WebGL.decompressionFallback = false;
+        
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+    }
+}
