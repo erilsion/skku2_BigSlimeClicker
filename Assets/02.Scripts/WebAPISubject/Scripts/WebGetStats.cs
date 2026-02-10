51e0893e07d5077598c23376435559cfb1ec30d4
erilsion <erilsion@gmail.com>
2026-02-10 17:09:05 +0900

    Feat: 메이플API 기초적 골자 잡아보기

---
+++ Assets/02.Scripts/WebAPISubject/Scripts/WebGetStats.cs
@@ -0,0 +1,24 @@
+﻿using Cysharp.Threading.Tasks;
+using System;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class WebGetStats : MonoBehaviour
+{
+    private const string API_KEY = "test_e3ff7acfde4849871d2d5c17a8b66c17028bbea2c0d0b370ab48a192b2fbf986efe8d04e6d233bd35cf2fabdeb93fb0d";
+
+    private async void Start()
+    {
+        string url = "https://open.api.nexon.com/maplestory/v1/character/basic?ocid=XXXX";
+        string result = await GetWebText(url);
+        Debug.Log(result);
+    }
+
+    private async UniTask<string> GetWebText(string url)
+    {
+        UnityWebRequest txt = UnityWebRequest.Get(url);
+        txt.SetRequestHeader("x-nxopen-api-key", API_KEY);
+        await txt.SendWebRequest();
+        return txt.downloadHandler.text;
+    }
+}
