64e4b63c95343fb51af4c81e326ec677c862f25d
erilsion <erilsion@gmail.com>
2026-02-11 10:51:44 +0900

    Feat: 메이플 캐릭터 세부 스탯 정보 가져오기 구현

--- Assets/02.Scripts/WebAPISubject/Scripts/WebGetStatus.cs
+++ Assets/02.Scripts/WebAPISubject/Scripts/WebGetStatus.cs
@@ -1,5 +1,7 @@
 ﻿using Cysharp.Threading.Tasks;
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -9,19 +11,26 @@ public class WebGetStatus : MonoBehaviour
 {
     private const string API_KEY = "test_e3ff7acfde4849871d2d5c17a8b66c17028bbea2c0d0b370ab48a192b2fbf986efe8d04e6d233bd35cf2fabdeb93fb0d";
     private MapleCharacter _character;
+    private MapleCharacterStat _characterStat;
 
-    [SerializeField] private TextMeshProUGUI _statusText;
+    [SerializeField] private TextMeshProUGUI _basicText;
+    [SerializeField] private TextMeshProUGUI _statText;
     [SerializeField] private RawImage _characterImage;
 
     private async void Start()
     {
-        string url = "https://open.api.nexon.com/maplestory/v1/character/basic?ocid=16b1a1f2502eea2a9fcf32c8eaa6b2ea";
-        string result = await GetWebText(url);
-        _character = JsonUtility.FromJson<MapleCharacter>(result);
-        _characterImage.texture = await GetWebTexture(_character.character_image);
+        string basic = "https://open.api.nexon.com/maplestory/v1/character/basic?ocid=16b1a1f2502eea2a9fcf32c8eaa6b2ea";
+        string stat = "https://open.api.nexon.com/maplestory/v1/character/stat?ocid=16b1a1f2502eea2a9fcf32c8eaa6b2ea";
+        string basicResult = await GetWebText(basic);
+        string statResult = await GetWebText(stat);
+        _character = JsonUtility.FromJson<MapleCharacter>(basicResult);
         string dateCreate = await PolishDataCreate(_character.character_date_create);
-        Debug.Log(result);
-        _statusText.text =
+        _characterStat = JsonUtility.FromJson<MapleCharacterStat>(statResult);
+        BindStatUI();
+        _characterImage.texture = await GetWebTexture(_character.character_image);
+        Debug.Log(basicResult);
+        Debug.Log(statResult);
+        _basicText.text =
         $"캐릭터 이름: {_character.character_name}\n" +
         $"서버: {_character.world_name}\n" +
         $"성별: {_character.character_gender}\n" +
@@ -29,6 +38,7 @@ public class WebGetStatus : MonoBehaviour
         $"레벨: {_character.character_level}\n" +
         $"길드: {_character.character_guild_name}\n" +
         $"생성일자: {dateCreate}";
+        // _statText.text = ui 바인딩;
     }
 
     private async UniTask<string> GetWebText(string url)
@@ -61,7 +71,35 @@ public class WebGetStatus : MonoBehaviour
         return dateOnly;
     }
 
-    [System.Serializable]
+    private void BindStatUI()
+    {
+        Dictionary<string, string> statDictionary = new Dictionary<string, string>();
+
+        foreach (var stat in _characterStat.final_stat)
+            statDictionary[stat.stat_name] = stat.stat_value;
+
+        _statText.text =
+            $"전투력 : {GetStat(statDictionary, "전투력")}\n" +
+            $"최소 공격력 : {GetStat(statDictionary, "최소 스탯공격력")}\n" +
+            $"최대 공격력 : {GetStat(statDictionary, "최대 스탯공격력")}\n\n" +
+
+            $"INT : {GetStat(statDictionary, "INT")}\n" +
+            $"STR : {GetStat(statDictionary, "STR")}\n" +
+            $"DEX : {GetStat(statDictionary, "DEX")}\n" +
+            $"LUK : {GetStat(statDictionary, "LUK")}\n\n" +
+
+            $"보스 데미지 : {GetStat(statDictionary, "보스 몬스터 데미지")}%\n" +
+            $"방무 : {GetStat(statDictionary, "방어율 무시")}%\n" +
+            $"크확 : {GetStat(statDictionary, "크리티컬 확률")}%\n" +
+            $"크뎀 : {GetStat(statDictionary, "크리티컬 데미지")}%";
+    }
+
+    private string GetStat(Dictionary<string, string> dict, string key)
+    {
+        return dict.TryGetValue(key, out var value) ? value : "0";
+    }
+
+    [Serializable]
     public class MapleCharacter
     {
         public string date;
@@ -78,5 +116,21 @@ public class WebGetStatus : MonoBehaviour
         public string character_date_create;
         public string access_flag;
         public string liberation_quest_clear;
+    }
+
+    [Serializable]
+    public class MapleCharacterStat
+    {
+        public string date;
+        public string character_class;
+        public List<MapleFinalStat> final_stat;
+        public int remain_ap;
+    }
+
+    [Serializable]
+    public class MapleFinalStat
+    {
+        public string stat_name;
+        public string stat_value;
     }
 }
